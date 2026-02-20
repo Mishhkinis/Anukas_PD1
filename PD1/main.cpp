37f7304d9f6a6ecaa82f59fc173f4ec27308a430
Mishhkinis <adrijusanukas@gmail.com>
2026-02-20 10:57:01 +0200

    kintamieji ir bibliotekos antras kartas

--- PD1/main.cpp
+++ PD1/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+{ int n, m;
+double GBP_Bendras=0.8729;
+double GBP_Pirkti=0.8600;
+double GBP_Parduoti=0.9220;
+double USD_Bendras=1.1793;
+double USD_Pirkti=1.1460;
+double USD_Parduoti=1.2340;
+double INR_Bendras=104.6918;
+double INR_Pirkti=101.3862;
+double INR_Parduoti=107.8546;
