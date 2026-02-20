a3c1a34c2ba310785ee592e5321a355d219fa868
Mishhkinis <adrijusanukas@gmail.com>
2026-02-20 11:02:01 +0200

    sukuriamas veiksmo pasirinkimas

--- PD1/main.cpp
+++ PD1/main.cpp
@@ -15,3 +15,11 @@ double USD_Parduoti=1.2340;
 double INR_Bendras=104.6918;
 double INR_Pirkti=101.3862;
 double INR_Parduoti=107.8546;
+    cout << "Pasirink veiksma" << endl;
+    cout << "1. Valiutos kurso palyginimas su kita valiuta" << endl;
+    cout << "2. Galimybe isigyti valiutos (t. y. pirkti valiuta)" << endl;
+    cout << "3. Galimybe parduoti valiuta (t. y. parduoti valiuta)." << endl;
+    cout << "Iveskite skaiciu (1-3): ";
+    cin >>n;
+    return 0;
+}
